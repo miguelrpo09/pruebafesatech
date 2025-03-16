aa9566cecf03823d34d9b044ec51d65388a27eb0
Miguelrpo09 <72237259+miguelrpo09@users.noreply.github.com>
2025-03-15 19:48:54 -0500

    .

--- js/main.js
+++ js/main.js
@@ -173,3 +173,26 @@ document.addEventListener('DOMContentLoaded', function() {
         carousel.parentNode.appendChild(clone); // Agrega el clon al final del contenedor
     });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+	const navHeight = document.querySelector('.main-nav').offsetHeight;
+
+	document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+		anchor.addEventListener('click', function (e) {
+			e.preventDefault();
+
+			const targetId = this.getAttribute('href');
+			const targetElement = document.querySelector(targetId);
+
+			if (targetElement) {
+				const targetPosition = targetElement.offsetTop;
+				const adjustedPosition = targetPosition - navHeight;
+
+				window.scrollTo({
+					top: adjustedPosition,
+					behavior: 'smooth' // Optional: Smooth scrolling animation
+				});
+			}
+		});
+	});
+});
